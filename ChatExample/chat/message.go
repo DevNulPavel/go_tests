26f73a4f14dd22405cd3060e74c4fdda69bab28f
Pavel Ershov <429573036@qip.ru>
2017-06-06 11:29:19 +0300

    Idea project

--- ChatExample/chat/message.go
+++ ChatExample/chat/message.go
@@ -1,12 +1,11 @@
 package chat
 
-
 type Message struct {
-    Author string `json:"author"`
-    Body   string `json:"body"`
+	Author string `json:"author"`
+	Body   string `json:"body"`
 }
 
 // конвертация в строку
 func (this *Message) String() string {
-    return this.Author + " says " + this.Body
+	return this.Author + " says " + this.Body
 }
--- ChatExample/main.go
+++ ChatExample/main.go
@@ -5,16 +5,16 @@ import "net/http"
 import "./chat"
 
 func main() {
-    log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
 
-    // websocket server
-    server := chat.NewServer("/websocket")
-    go server.Listen()
+	// websocket server
+	server := chat.NewServer("/websocket")
+	go server.Listen()
 
-    // HTTP Server
-    // static files
-    http.Handle("/", http.FileServer(http.Dir("web")))
-    // Listen
-    error := http.ListenAndServe(":8080", nil)
-    log.Fatal(error)
+	// HTTP Server
+	// static files
+	http.Handle("/", http.FileServer(http.Dir("web")))
+	// Listen
+	error := http.ListenAndServe(":8080", nil)
+	log.Fatal(error)
 }
