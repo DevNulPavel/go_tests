28d1a41595bc89100a8ff4073afc00ee0951cbad
Pavel Ershov <429573036@qip.ru>
2017-06-16 17:53:23 +0300

    Profiling enabled

--- GameServer_3/main.go
+++ GameServer_3/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "log"
-import "./game_server"
+import (
+    "log"
+    "time"
+    "github.com/pkg/profile"
+    "./game_server"
+)
 
 func main() {
+    defer profile.Start(profile.CPUProfile, profile.ProfilePath("."), profile.NoShutdownHook).Stop()
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
 
 	// Запуск сервера
 	server := gameserver.NewServer()
-	server.StartSyncListen()
+	go server.StartSyncListen()
+
+    <-time.After(time.Second * 30)
+    server.ExitServer()
 }
