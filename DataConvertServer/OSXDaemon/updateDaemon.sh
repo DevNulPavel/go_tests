1167ca98840267b93f0bf27d6939a85381d497ce
Pavel Ershov <devnulpavel@gmail.com>
2017-11-13 15:51:15 +0300

    Small improvements

--- DataConvertServer/OSXDaemon/updateDaemon.sh
+++ DataConvertServer/OSXDaemon/updateDaemon.sh
@@ -6,5 +6,5 @@ PLIST_PATH=$PLIST_FOLDER/$PLIST_NAME
 
 sudo launchctl unload $PLIST_PATH
 sudo cp $PLIST_NAME $PLIST_PATH
-sudo chmod 755 $PLIST_PATH
+sudo chmod 644 $PLIST_PATH
 sudo chown root:wheel $PLIST_PATH
