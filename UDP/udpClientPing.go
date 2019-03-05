980140c681f77415245be1c66fab17e2200e52a7
Pavel Ershov <devnulpavel@gmail.com>
2019-03-05 16:04:18 +0300

    UDP example improvement

--- UDP/udpClientPing.go
+++ UDP/udpClientPing.go
@@ -3,90 +3,100 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
 	"time"
 )
 
+const serverAddressString = "127.0.0.1:9999" // "127.0.0.1:9999" "devnulpavel.ddns.net:9999"
+
 func rawClient() {
 	// Определяем адрес
-	address, err := net.ResolveUDPAddr("udp", "devnulpavel.ddns.net:9999") // devnulpavel.ddns.net 	192.168.1.3
+	serverAddress, err := net.ResolveUDPAddr("udp", serverAddressString)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Resolved ip address: %s\n", address)
+	log.Printf("Resolved ip address: %s\n", serverAddress)
 
 	// Подключение к серверу
-	c, err := net.DialUDP("udp", nil, address)
+	conn, err := net.DialUDP("udp", nil, serverAddress)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer c.Close()
-
-	readErrorCounter := 0
+	defer conn.Close()
 
 	const dataSize = 1024
-    const timeBegin = 0
-    const counterBegin = 200
-    data := make([]byte, dataSize)
-	var counter uint64 = 0
-
+	const timeOffset = 0
+	const counterOffset = 200
 	const requestsCount = 100
+
+	var counter uint64 = 1 // По-умолчанию 0
+	data := make([]byte, dataSize)
+	readErrorCounter := 0
 	startTime := time.Now()
+
 	for i := 0; i < requestsCount; i++ {
+		// Формируем данные
 		sendTime := uint64(time.Now().UnixNano())
-		binary.BigEndian.PutUint64(data[timeBegin:timeBegin+8], sendTime)
-		binary.BigEndian.PutUint64(data[counterBegin:counterBegin+8], counter)
+		binary.BigEndian.PutUint64(data[timeOffset:timeOffset+8], sendTime)
+		binary.BigEndian.PutUint64(data[counterOffset:counterOffset+8], counter)
 
 		// Пытаемся записать данные
-		c.SetWriteDeadline(time.Now().Add(500 * time.Millisecond))
-		currentWritten, err := c.Write(data)
+		conn.SetWriteDeadline(time.Now().Add(500 * time.Millisecond))
+		currentWritten, err := conn.Write(data)
 		if err != nil {
 			if err, ok := err.(net.Error); ok && err.Timeout() {
-				fmt.Printf("Is WRITE timeout error: %s\n", err)
+				log.Printf("Is WRITE timeout error: %s\n", err)
 				continue
 			} else {
-				fmt.Println(err)
+				log.Println(err)
 				return
 			}
 			return
 		} else if currentWritten < dataSize {
-			fmt.Printf("Written less bytes - %d\n", currentWritten)
+			log.Printf("Written less bytes - %d\n", currentWritten)
 			return
 		}
 
-		// теперь читаем
-		c.SetReadDeadline(time.Now().Add(5000 * time.Millisecond))
-		receivedCount, senderAddress, err := c.ReadFromUDP(data)
+		// Теперь читаем
+		conn.SetReadDeadline(time.Now().Add(5000 * time.Millisecond))
+		receivedCount, receivedServerAddress, err := conn.ReadFromUDP(data)
 		if err != nil {
 			if err, ok := err.(net.Error); ok && err.Timeout() {
-				fmt.Printf("Is READ timeout error: %s\n", err)
+				log.Printf("Is READ timeout error: %s\n", err)
 				readErrorCounter++
 				if readErrorCounter > 5 {
-					fmt.Println("Disconnected by many read timeouts")
+					log.Println("Disconnected by many read timeouts")
 					return
 				} else {
 					continue
 				}
 			} else {
-				fmt.Printf("Read error: %s\n", err)
+				log.Printf("Read error: %s\n", err)
 				return
 			}
 		} else if receivedCount == 0 {
-			fmt.Println("Disconnected")
+			log.Println("Disconnected")
 			return
+		} else if receivedServerAddress.IP.Equal(serverAddress.IP) == false {
+			log.Printf("Invalid received server address: %s, must be: %s", receivedServerAddress, serverAddress)
+			continue
 		} else if receivedCount < dataSize {
-			fmt.Printf("Received less data size - %d\n", receivedCount)
+			log.Printf("Received less data size - %d\n", receivedCount)
 		}
+
 		// Reset read counter error
 		readErrorCounter = 0
 
-		receivedSendTimeUint64 := binary.BigEndian.Uint64(data[timeBegin:timeBegin+8])
-		receivedCounterUint64 := binary.BigEndian.Uint64(data[counterBegin:counterBegin+8])
+		// Парсим прочитанные данные
+		receivedSendTimeUint64 := binary.BigEndian.Uint64(data[timeOffset : timeOffset+8])
+		receivedCounterUint64 := binary.BigEndian.Uint64(data[counterOffset : counterOffset+8])
 
+		// Проверяем валидность данных
 		if receivedCounterUint64 != counter {
-			fmt.Println("Receive counter error")
+			log.Println("Receive counter error")
 			continue
 		}
 
@@ -95,16 +105,16 @@ func rawClient() {
 		// Ping
 		receivedSendTime := time.Unix(0, int64(receivedSendTimeUint64))
 		ping := float64(time.Now().Sub(receivedSendTime).Nanoseconds()) / 1000.0 / 1000.0
-		fmt.Printf("Ping = %fms, from adress: %s\n", ping, senderAddress)
+		fmt.Printf("Ping = %fms, from adress: %s\n", ping, serverAddress)
 
 		//time.Sleep(1000 * time.Millisecond)
 	}
 
-    endTime := time.Now()
-    duration := endTime.Sub(startTime).Seconds()
+	endTime := time.Now()
+	duration := endTime.Sub(startTime).Seconds()
 
-    requestsPerSec := requestsCount/duration
-    fmt.Printf("Requests per sec value: %f", requestsPerSec)
+	requestsPerSec := requestsCount / duration
+	fmt.Printf("Requests per sec value: %f", requestsPerSec)
 }
 
 func main() {
--- UDP/udpServerPing.go
+++ UDP/udpServerPing.go
@@ -1,64 +1,175 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
+	"io"
+	"log"
 	"net"
+	"sync"
 	"time"
 )
 
-func HandleServerConnectionRaw(c *net.UDPConn, address *net.UDPAddr) {
-	defer c.Close()
+var usersMutex sync.Mutex
+var users map[string]*User
 
-	const dataSize = 1024*64
+//////////////////////////////////////////////////////////////////////
+
+// User ???
+type User struct {
+	clientAddress    *net.UDPAddr
+	lastActiveTime   time.Time
+	dataMutex        sync.Mutex
+	lastPacketNumber uint64
+}
+
+func (user *User) handleData(data []byte) {
+	const counterOffset = 200
+
+	if user == nil {
+		log.Printf("User do not exists")
+	}
+
+	if len(data) < counterOffset+8 {
+		log.Printf("Invalid UDP packages size: %d", len(data))
+		return
+	}
+
+	// Парсим прочитанные данные
+	receivedPacketNumber := binary.BigEndian.Uint64(data[counterOffset : counterOffset+8])
+	//log.Printf("Received package number: %d", receivedPacketNumber)
+
+	// Может быть уже получали пакет или это более старый?
+	if receivedPacketNumber <= user.lastPacketNumber {
+		log.Printf("Invalid UDP packages seq: %d received, %d last", receivedPacketNumber, user.lastPacketNumber)
+		return
+	}
+
+	// Это следующий пакет, все ок
+	user.dataMutex.Lock()
+	user.lastPacketNumber = receivedPacketNumber
+	user.dataMutex.Unlock()
+}
+
+//////////////////////////////////////////////////////////////////////
+
+func getUserForAddr(addr *net.UDPAddr) *User {
+	usersMutex.Lock()
+	defer usersMutex.Unlock()
+
+	// Ищем пользователя, если нету - создаем нового
+	user, ok := users[addr.String()]
+	if ok {
+		user.lastActiveTime = time.Now()
+		return user
+	}
+
+	// Создание нового
+	newUser := &User{
+		clientAddress:    addr,
+		lastActiveTime:   time.Now(),
+		lastPacketNumber: 0,
+	}
+	users[addr.String()] = newUser
+	return newUser
+}
+
+// Функция чистит юзеров от которых давно не было ответа
+func clearOldUsers() {
+	const clearTime = 20 * time.Second
+	const oldUserDuration = 15 * time.Second
+
+	timer := time.NewTimer(clearTime)
+	for {
+		<-timer.C
+		timer.Reset(clearTime)
+
+		//log.Printf("Old user clear tick")
+
+		usersMutex.Lock()
+		deleteKeys := make([]string, 0)
+		for key, val := range users {
+			testTime := val.lastActiveTime.Add(oldUserDuration)
+			if testTime.Before(time.Now()) {
+				deleteKeys = append(deleteKeys, key)
+			}
+		}
+		for _, key := range deleteKeys {
+			delete(users, key)
+			log.Printf("User for address %s deleted", key)
+		}
+		usersMutex.Unlock()
+	}
+}
+
+func handleServerConnectionRaw(c *net.UDPConn, serverAddress *net.UDPAddr, wg *sync.WaitGroup, threadIndex int) {
+	defer wg.Done()
+
+	const dataSize = 1024
 	udpBuffer := make([]byte, dataSize)
 
 	for {
-		timeVal := time.Now().Add(5 * time.Minute)
-		c.SetDeadline(timeVal)
-
-		readCount, receiveAddress, err := c.ReadFromUDP(udpBuffer)
-		if err != nil {
-			fmt.Println(err)
-			return
-		} else if readCount == 0 {
-			fmt.Println("Disconnected")
-			return
+		// Читаем данные в буффер
+		readCount, clientAddress, err := c.ReadFromUDP(udpBuffer)
+		if (err == io.EOF) || (readCount == 0) { // Нужно ли проверять readCount
+			log.Println("Disconnected")
+			continue
+		} else if err != nil {
+			log.Println(err)
+			continue
 		}
+
+		//log.Printf("Handle message in thread: %d", threadIndex)
+
+		// Получаем юзера для текущего адреса
+		user := getUserForAddr(clientAddress)
+		user.handleData(udpBuffer[0:readCount])
 
 		// Теперь очередь ответной записи
-		writtenCount, err := c.WriteToUDP(udpBuffer[0:readCount], receiveAddress)
+		writtenCount, err := c.WriteToUDP(udpBuffer[0:readCount], clientAddress)
 		if err != nil {
-			fmt.Println(err)
-			return
+			log.Println(err)
+			continue
 		} else if writtenCount < readCount {
-			fmt.Printf("Written less bytes - %d from \n", writtenCount, readCount)
-			return
+			log.Printf("Written less bytes - %d from \n", writtenCount, readCount)
+			continue
 		}
 	}
-}
-
-func server() {
-	// Определяем адрес
-	address, err := net.ResolveUDPAddr("udp", ":9999")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Прослушивание сервера
-	connection, err := net.ListenUDP("udp", address)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Print("Server started\n")
-	HandleServerConnectionRaw(connection, address)
 }
 
 func main() {
-	go server()
+	users = make(map[string]*User)
 
-	var input string
-	fmt.Scanln(&input)
+	// Определяем адрес
+	address, err := net.ResolveUDPAddr("udp", ":9999")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Горутина удаления старых пользователей
+	go clearOldUsers()
+
+	// Создание соединения
+	connection, err := net.ListenUDP("udp", address)
+	if err == nil {
+		log.Print("New connection created\n")
+
+		// Выставляем размер буффера
+		//connection.SetReadBuffer(1024)
+		//connection.SetWriteBuffer(1024)
+
+		// Создаем горутины, которые обрабатывают данные
+		const goroutinesCount = 10
+		var wg sync.WaitGroup
+		for i := 0; i < goroutinesCount; i = i + 1 {
+			wg.Add(1)
+			go handleServerConnectionRaw(connection, address, &wg, i)
+		}
+		wg.Wait()
+	} else {
+		log.Println("Error in accept: %s", err.Error())
+	}
+
+	// Закрытиие соединения
+	connection.Close()
 }
